29aa74c4777313898400b1a4e48be9cb18e2f1d7
Sanjay Das <Sanjay@Sanjays-MacBook-Pro.local>
2017-09-12 21:34:02 +0530

    Refactored from scratch, new GrainClass not depending on p5.soundFile

--- 02_GranularSynth/granular_synth.js
+++ 02_GranularSynth/granular_synth.js
@@ -1,49 +1,174 @@
-var mySound;
-var playrate;
-var voices = 0;
-function preload() {
+//Granular Synth Prototype for Aesthesis
+//philterSoup 2017
 
-  mySound = loadSound('audioSource.mp3');
+var ac;
+var buffer;
+var loadState = false;
 
+var playRate, dur = 2;
+var voices = [];
+var maxVoice = 50;
+
+var delay;
+
+var bgCol = 127;
+
+var fft, grainMix;
+
+
+function getData() {
+
+  // source = ac.createBufferSource();
+  var request = new XMLHttpRequest();
+
+  request.open('GET', 'Ambient_track_2.mp3', true);
+
+  request.responseType = 'arraybuffer';
+
+
+  request.onload = function() {
+  var audioData = request.response;
+
+  ac.decodeAudioData(audioData, function(buf){
+    buffer = buf;
+    loadState = true;
+    bgCol = 3;
+    loop();
+    // initGrains();
+  },
+
+      function(e){ console.log("Error with decoding audio data" + e.err); });
+
+  }
+
+  request.send();
 }
 
 function setup() {
-playRate = mouseY/height;
 
-reverb = new p5.Reverb();
- mySound.disconnect(); // so we'll only hear reverb...
+  createCanvas(windowWidth,windowHeight);
+  ac = getAudioContext();
 
- // connect soundFile to reverb, process w/
- // 3 second reverbTime, decayRate of 2%
- reverb.process(mySound, 3, 10);
- reverb.drywet(0.5);
+  grainMix = new p5.Gain();
+  grainMix.connect();
 
-if(mySound.isLoaded){
+  getData();
 
-  mySound.play();
-  mySound.loop(0.1,playRate/10,0.65,mySound.duration()/4,mySound.duration()/100);
-  }
+  playRate = mouseY/height;
+  // reverb = new p5.Reverb();
+  // reverb.process(grainMix, 5, 5);
+  // reverb.drywet(0.35);
 
+  // delay = new p5.Delay();
+  // delay.setType('pingPong');
+  //
+  // delay.process(grainMix,.65,.50,2000);
+  // delay.drywet(0.60);
+
+  textSize(32);
+  textAlign('center');
+  text("Loading...", width/2, height/2);
+
+  fft = new p5.FFT();
+  fft.setInput(grainMix);
+  noLoop();
+  // frameRate(30);
 }
 
 function draw(){
-  // playRate = map(mouseY,0,height,-0.75,0.65)/20;
-  playRate = (2 * (mouseY/height - 2 ) / 20 ) || 0.001 ;
-  mySound.rate(playRate);
 
+  background(bgCol,100);
+
+  playRate = 1 - mouseY/height;
+
+  drawCircles();
+}
+function drawCircles(){
+
+  var spectrum = fft.analyze();
+  for(var i = 0; i < spectrum.length; i+=20){
+  noFill();
+  stroke(spectrum[i]);
+  ellipse(mouseX + spectrum[i]/120,mouseY+spectrum[i]/120 ,spectrum[i] * 2,spectrum[i]* 2);
+  }
+}
+
+
+function Voice(){
+//Voice Handler Class
+}
+
+Voice.prototype.playGrain = function(){
+  this.grains = [];
+  this.grainCount = 0;
+
+  var that = this;
+
+  this.play = function(){
+
+    var grain = new Grain(mouseY/height * buffer.duration, mouseX/width * dur);
+
+    that.grains[that.grainCount] = grain;
+    that.grainCount += 1;
+
+    if(that.grainCount>10){
+      that.grainCount = 0;
+    }
+
+    that.timeOut = setTimeout(that.play,75);
+  }
+  if(loadState)
+  this.play();
+}
+
+Voice.prototype.stopGrain = function(){
+  clearTimeout(this.timeOut);
+}
+
+function Grain(offset,duration){
+//Grain Class
+  // console.log('hello');
+
+  var that = this;
+  this.now = ac.currentTime;
+  this.seed = Math.random();
+  this.amp = 0.65;
+  this.offset = offset;
+  this.duration = duration;
+
+  this.sound = ac.createBufferSource();
+  this.sound.buffer = buffer;
+  this.sound.playbackRate.value = this.seed * 4* playRate;
+  this.sound.disconnect();
+
+  this.gain = ac.createGain();
+
+  this.sound.connect(this.gain);
+  this.gain.connect(grainMix);
+
+  this.sound.start(this.now,this.offset, this.duration);
+  this.gain.gain.setValueAtTime(0.0, this.now);
+	this.gain.gain.linearRampToValueAtTime(this.amp,this.now + 4 * this.seed);
+	this.gain.gain.linearRampToValueAtTime(0,this.now + this.duration  );
+
+  this.sound.stop(this.now + this.duration + 0.1);
+	var tms = this.duration * 1000;
+	setTimeout(function(){
+		that.gain.disconnect();
+	},tms + 200);
 
 }
 
 function mousePressed(){
-voices++;
-mySound.loop(0.1,playRate,0.35,Math.random()*mySound.duration(),mySound.duration()/100 * Math.random());
-
-if(voices>5){
-mySound.stop();
-voices = 0;
+  var v = new Voice();
+  v.playGrain();
+  voices.push(v);
+  return false;
 }
 
+function mouseReleased(){
+  for(var i = 0; i < voices.length ; i++){
+    voices[i].stopGrain();
+    voices.splice(i);
+  }
 }
-// function mouseReleased()(){
-//   mySound.stop();
-// }
